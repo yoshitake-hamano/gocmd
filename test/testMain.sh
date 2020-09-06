d039e63550ca76415e99f458e33a80b4513213d8
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2020-09-06 22:57:28 +0900

    Implement assertIntEquels and assertStringEquals

--- test/testMain.sh
+++ test/testMain.sh
@@ -4,5 +4,53 @@ BIN_DIR=../bin
 HELLO=${BIN_DIR}/hello
 YACC=${BIN_DIR}/yacc
 
-${HELLO}
-${YACC} "1 + 2"
+function dumpStack() {
+    local i=0
+    local line_no
+    local function_name
+    local file_name
+    while caller $i ;do ((i++)) ;done | while read line_no function_name file_name;do echo "\t$file_name:$line_no\t$function_name" ;done >&2
+}
+
+function flunk() {
+    dumpStack
+    exit 1
+}
+
+function assertStringEquals() {
+    expect="${1}"
+    actual="${2}"
+    if [ "${expect}" != "${actual}" ]; then
+        echo "assertStringEquals fails"
+        echo "expect = ${expect}, actual = ${actual}"
+        flunk
+    fi
+}
+
+function assertIntEquals() {
+    expect=${1}
+    actual=${2}
+    if [ ! ${expect} -eq ${actual} ]; then
+        echo "assertIntEquals fails"
+        echo "expect = ${expect}, actual = ${actual}"
+        flunk
+    fi
+}
+
+assertStringEquals "abc" "abc"
+
+function testHello() {
+    s=`${HELLO}`
+    assertIntEquals 0 ${?}
+    assertStringEquals "Hello" "${s}"
+}
+
+function testYacc() {
+    s=`${YACC} "1 + 2"`
+    echo "${s}"
+    assertIntEquals 0 ${?}
+    assertStringEquals "main.BinOpExpr{left:main.NumExpr{literal:\"1\"}, operator:43, right:main.NumExpr{literal:\"2\"}}" "${s}"
+}
+
+testHello
+testYacc
