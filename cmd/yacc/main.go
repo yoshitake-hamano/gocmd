3ca21c67cb2b65a0156c9b6a1704a29c0aed3d09
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2020-09-12 23:22:55 +0900

    Devide parser.go.y

---
+++ cmd/yacc/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "fmt"
+    "text/scanner"
+    "os"
+    "strings"
+)
+
+type Lexer struct {
+    scanner.Scanner
+    result Expression
+}
+
+func (l *Lexer) Lex(lval *yySymType) int {
+    token := int(l.Scan())
+    if token == scanner.Int {
+        token = NUMBER
+    }
+    lval.token = Token{token: token, literal: l.TokenText()}
+    return token
+}
+
+func (l *Lexer) Error(e string) {
+    panic(e)
+}
+
+func main() {
+    l := new(Lexer)
+    l.Init(strings.NewReader(os.Args[1]))
+    yyParse(l)
+    fmt.Printf("%#v\n", l.result)
+}
