328e7980f8497da12b7faaf47b6ca2581131541c
Yoshitake Hamano <oed0cow6oy5@gmail.com>
2020-09-13 14:40:03 +0900

    Divide file

--- cmd/yacc/main.go
+++ cmd/yacc/main.go
@@ -1,33 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "text/scanner"
-    "os"
-    "strings"
+	"fmt"
+	"text/scanner"
+	"os"
+	"strings"
 )
 
 type Lexer struct {
-    scanner.Scanner
-    result Expression
+	scanner.Scanner
+	result Expression
 }
 
 func (l *Lexer) Lex(lval *yySymType) int {
-    token := int(l.Scan())
-    if token == scanner.Int {
-        token = NUMBER
-    }
-    lval.token = Token{token: token, literal: l.TokenText()}
-    return token
+	token := int(l.Scan())
+	if token == scanner.Int {
+		token = NUMBER
+	}
+	lval.token = Token{token: token, literal: l.TokenText()}
+	return token
 }
 
 func (l *Lexer) Error(e string) {
-    panic(e)
+	panic(e)
 }
 
 func main() {
-    l := new(Lexer)
-    l.Init(strings.NewReader(os.Args[1]))
-    yyParse(l)
-    fmt.Printf("%#v\n", l.result)
+	l := new(Lexer)
+	l.Init(strings.NewReader(os.Args[1]))
+	yyParse(l)
+	fmt.Printf("%#v\n", l.result)
+
+	r := l.result
+	switch r.(type) {
+	case BinOpExpr:
+		left := r.(BinOpExpr).left
+		right := r.(BinOpExpr).right
+		fmt.Printf("%#v\n", left.(NumExpr).literal)
+		fmt.Printf("%#v\n", right)
+	}
 }
--- cmd/yacc/parser.go.y
+++ cmd/yacc/parser.go.y
@@ -1,12 +1,8 @@
 %{
-package main
 
-import (
-    "fmt"
-    "text/scanner"
-    "os"
-    "strings"
-)
+// C language BNF
+// https://cs.wmich.edu/~gupta/teaching/cs4850/sumII06/The%20syntax%20of%20C%20in%20Backus-Naur%20form.htm
+package main
 
 type Expression interface{}
 type Token struct {
@@ -55,28 +51,3 @@ expr
     }
 
 %%
-
-type Lexer struct {
-    scanner.Scanner
-    result Expression
-}
-
-func (l *Lexer) Lex(lval *yySymType) int {
-    token := int(l.Scan())
-    if token == scanner.Int {
-        token = NUMBER
-    }
-    lval.token = Token{token: token, literal: l.TokenText()}
-    return token
-}
-
-func (l *Lexer) Error(e string) {
-    panic(e)
-}
-
-func main() {
-    l := new(Lexer)
-    l.Init(strings.NewReader(os.Args[1]))
-    yyParse(l)
-    fmt.Printf("%#v\n", l.result)
-}
